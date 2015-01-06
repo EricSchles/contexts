74a34ab0df58b468a8940dbc61f285b09552a617
EricSchles <ericschles@gmail.com>
2015-01-06 09:20:46 -0500

    updated models

--- slavery_website/cms/models.py
+++ slavery_website/cms/models.py
@@ -5,3 +5,15 @@ def get_entries():
 		cur = con.cursor()
 		entries = cur.execute("select * from entries").fetchall()
 		return entries
+
+def add_entry(entry):
+	with sqlite3.connect("database.db") as con:
+		cur = con.cursor()
+		cur.execute("insert into entries (entry) values (?)", entry)
+		con.commit()
+
+def remove_entry(id):
+	with sqlite3.connect("database.db") as con:
+		cur = con.cursor()
+		query = "delete from entries where id = '%s';" % id.strip()
+		cur.execute(query)
